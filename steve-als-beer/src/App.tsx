c580c46e49a80d4a3320fff2ab2a9702d557d772
Ryan Burtson <rburtson@udel.edu>
2024-12-19 03:45:40 -0500

    Test

--- steve-als-beer/src/App.tsx
+++ steve-als-beer/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App" style={{ textAlign: "center" }}>
       <header className="App-header">
-        <h1>Welcome to Steve and Al's Brewhaus!</h1>
+        <a href="https://www.angelfire.com/pa/beerandweather/beerecipes.html"><h1>Welcome to Steve and Al's Brewhaus!</h1></a>
         <div>Currently being Recreated by: Ryan Burtson</div>
         <div>Last update: December 2024</div>
       </header>
