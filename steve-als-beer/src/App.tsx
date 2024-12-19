d29de9987f957404503314791fdf86ac3fd63db7
Ryan Burtson <rburtson@udel.edu>
2024-12-19 03:26:08 -0500

    Test2

--- steve-als-beer/src/App.tsx
+++ steve-als-beer/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App" style={{ textAlign: "center" }}>
       <header className="App-header">
-        <h1>Welcome to Steve and Al's Brewhaus</h1>
-        <div>Currently being Recreated by Ryan Burtson</div>
+        <h1>Welcome to Steve and Al's Brewhaus!</h1>
+        <div>Currently being Recreated by: Ryan Burtson</div>
         <div>Last update: December 2024</div>
       </header>
     </div>
