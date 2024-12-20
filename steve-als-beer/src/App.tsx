5b30614d13cfba07aeb950e4e27ca99169e05de1
Ryan Burtson <rburtson@udel.edu>
2024-12-20 01:00:53 -0500

    Bug Fix

--- steve-als-beer/src/App.tsx
+++ steve-als-beer/src/App.tsx
@@ -24,7 +24,6 @@ function App() {
   const [hoveredItem, setHoveredItem] = useState<string | null>(null);
   const [hoveredColumn, setHoveredColumn] = useState<string | null>(null);
   const [tooltipPosition, setTooltipPosition] = useState<{ top: number, left: number }>({ top: 0, left: 0 });
-  const [page, setPage] = useState<string>("Home");
 
   const handleMouseEnter = (item: string, column: string, e: React.MouseEvent) => {
     const rect = e.currentTarget.getBoundingClientRect(); // Get the bounding box of the image
@@ -49,13 +48,13 @@ function App() {
           <BubbleEffect />
           <div className="grid-container">
             <div className="left-column">
-                <Link to = "mash"><img src={mash} alt="mash" className="clickable" onClick={() => setPage("mash")} onMouseEnter={(e) => handleMouseEnter("mash", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "mash"><img src={mash} alt="mash" className="clickable" onMouseEnter={(e) => handleMouseEnter("mash", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "mash" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Mashing Info</div>}
-                <Link to = "grains"><img src={grain} alt="grains" className="clickable" onClick={() => setPage("grain")} onMouseEnter={(e) => handleMouseEnter("grains", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "grains"><img src={grain} alt="grains" className="clickable" onMouseEnter={(e) => handleMouseEnter("grains", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "grains" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Grain Types</div>}
-                <Link to = "hops"><img src={hops} alt="hops" className="clickable" onClick={() => setPage("hops")} onMouseEnter={(e) => handleMouseEnter("hops", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "hops"><img src={hops} alt="hops" className="clickable" onMouseEnter={(e) => handleMouseEnter("hops", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "hops" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Hops</div>}
-                <Link to = "yeast"><img src={yeast} alt="yeast" className="clickable" onClick={() => setPage("yeast")} onMouseEnter={(e) => handleMouseEnter("yeast", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "yeast"><img src={yeast} alt="yeast" className="clickable" onMouseEnter={(e) => handleMouseEnter("yeast", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "yeast" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Yeast</div>}
             </div>
             <div className="center-content">
@@ -72,13 +71,13 @@ function App() {
               </Routes>
               </div>
             <div className="right-column">
-                <Link to = "additives"><img src={add} alt="additives" className="clickable" onClick={() => setPage("additives")} onMouseEnter={(e) => handleMouseEnter("additives", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "additives"><img src={add} alt="additives" className="clickable" onMouseEnter={(e) => handleMouseEnter("additives", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "additives" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Additives</div>}
-                <Link to = "recipes"><img src={recipe} alt="recipes" className="clickable" onClick={() => setPage("recipes")} onMouseEnter={(e) => handleMouseEnter("recipes", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "recipes"><img src={recipe} alt="recipes" className="clickable" onMouseEnter={(e) => handleMouseEnter("recipes", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "recipes" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Recipes</div>}
-                <Link to = "styles"><img src={styles} alt="styles" className="clickable" onClick={() => setPage("styles")} onMouseEnter={(e) => handleMouseEnter("styles", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "styles"><img src={styles} alt="styles" className="clickable" onMouseEnter={(e) => handleMouseEnter("styles", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "styles" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Beer Styles</div>}
-                <Link to = "glossary"><img src={glossary} alt="glossary" className="clickable" onClick={() => setPage("glossary")} onMouseEnter={(e) => handleMouseEnter("glossary", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
+                <Link to = "glossary"><img src={glossary} alt="glossary" className="clickable" onMouseEnter={(e) => handleMouseEnter("glossary", "left", e)} onMouseLeave={handleMouseLeave}/></Link>
                 {hoveredItem === "glossary" && hoveredColumn === "left" && <div className="textbox" style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>Glossary</div>}
             </div>
           </div>
